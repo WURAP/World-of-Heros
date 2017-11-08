26f066923e18cb0631579daca96d763355d34608
mariav25 <32909814+mariav25@users.noreply.github.com>
2017-11-08 17:55:17 +0100

    Update verifyHash.java

--- Login-Aufgaben/AP7/verifyHash.java
+++ Login-Aufgaben/AP7/verifyHash.java
@@ -1,6 +1,10 @@
 package bvb
 
-import 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import bvb.User
 import bvb.Constants
